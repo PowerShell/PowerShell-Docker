9fe6b48c62e243edbd12b34a8606d5038719f1d0
Travis Plunk <travis.plunk@microsoft.com>
2018-05-24 12:57:17 -0700

    add copyright and comments

--- release/nanoserver/getLatestTag.ps1
+++ release/nanoserver/getLatestTag.ps1
@@ -1,19 +1,34 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+# return objects representing the tags we need to base the nanoserver image on
+
+# The versions of nanoserver we care about
 $shortTags = @('1709','1803')
 $results = @()
+
+# Get all the tage
 $tags = Invoke-RestMethod https://registry.hub.docker.com/v1/repositories/microsoft/nanoserver/tags
+
 foreach($shortTag in $shortTags)
 {
+    # filter to tags we care about
+    # then, to full tags
+    # then get the newest tag
     $fullTag = $tags | 
         Where-Object{$_.name -like "${shortTag}*"} |
             Where-Object{$_.name -match '\d{4}_KB\d{7}'} | 
                 Sort-Object -Descending -Property name | 
                     Select-Object -ExpandProperty name -First 1 
 
+    # Return the short form of the tag
     $results += [PSCustomObject] @{
         Type = 'Short'
         Tag = $shortTag
         FromTag = $fullTag
     }
+
+    # Return the full form of the tag
     $results += [PSCustomObject] @{
         Type = 'Full'
         Tag = $fullTag
