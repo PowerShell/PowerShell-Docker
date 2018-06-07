e5036c16d7101cfe804f9b28deb72da0e0a4bb60
Travis Plunk <travis.plunk@microsoft.com>
2018-06-07 15:40:40 -0700

    correct path to common module in get latest tags script

--- release/stable/ubuntu16.04/getLatestTag.ps1
+++ release/stable/ubuntu16.04/getLatestTag.ps1
@@ -7,7 +7,7 @@
 $shortTags = @('xenial')
 
 $parent = Join-Path -Path $PSScriptRoot -ChildPath '..'
-$repoRoot = Join-Path -Path $parent -ChildPath '..'
+$repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath '..'
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
