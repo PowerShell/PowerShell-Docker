99739d9c299a3f355f7b250fd6253de32c948898
Travis Plunk <travis.plunk@microsoft.com>
2018-06-25 17:05:03 -0700

    add `preview-*` tags for all preview images (#20)

--- createAllManifests.ps1
+++ createAllManifests.ps1
@@ -2,45 +2,38 @@
 # Licensed under the MIT License.
 
 # script to create the docker manifest lists
-# Update Stable and Preview version
 param (
     [ValidateNotNullOrEmpty()]
     [string]
-    $Registry = 'microsoft',
-    [ValidatePattern('\d+\.\d+\.\d+')]
-    [string]
-    $StableVersion = '6.0.2',
-    [string]
-    [ValidatePattern('\d+\.\d+\.\d+\-\w+\.?\d*')]
-    $PreviewVersion = '6.1.0-preview.3'
+    $Registry = 'microsoft'
 )
 
 $createScriptPath = Join-Path -Path $PSScriptRoot -ChildPath 'createManifest.ps1'
 
 
-$latestStableUbuntu   = "$stableVersion-ubuntu-xenial"
-$latestStableWsc1709  = "$stableVersion-windowsservercore-1709"
-$latestStableWscLtsc  = "$stableVersion-windowsservercore-latest"
-$latestStableWsc1803  = "$stableVersion-windowsservercore-1803"
-$latestStableNano1709 = "$stableVersion-nanoserver-1709"
-$latestStableNano1803 = "$stableVersion-nanoserver-1803"
+$latestStableUbuntu   = "ubuntu-xenial"
+$latestStableWsc1709  = "windowsservercore-1709"
+$latestStableWscLtsc  = "windowsservercore-latest"
+$latestStableWsc1803  = "windowsservercore-1803"
+$latestStableNano1709 = "nanoserver-1709"
+$latestStableNano1803 = "nanoserver-1803"
 
-$latestPreviewUbuntu    = "$previewVersion-ubuntu-xenial"
-$latestPreviewWsc1709  = "$previewVersion-windowsservercore-1709"
-$latestPreviewWscLtsc  = "$previewVersion-windowsservercore-latest"
-$latestPreviewWsc1803  = "$previewVersion-windowsservercore-1803"
-$latestPreviewNano1709 = "$previewVersion-nanoserver-1709"
-$latestPreviewNano1803 = "$previewVersion-nanoserver-1803"
+$latestPreviewUbuntu   = "preview-ubuntu-xenial"
+$latestPreviewWsc1709  = "preview-windowsservercore-1709"
+$latestPreviewWscLtsc  = "preview-windowsservercore-latest"
+$latestPreviewWsc1803  = "preview-windowsservercore-1803"
+$latestPreviewNano1709 = "preview-nanoserver-1709"
+$latestPreviewNano1803 = "preview-nanoserver-1803"
 
 &$createScriptPath -ContainerRegistry $Registry -taglist $latestPreviewUbuntu, $latestPreviewWsc1709, $latestPreviewWscLtsc, $latestPreviewWsc1803  -ManifestTag 'preview'
 &$createScriptPath -ContainerRegistry $Registry -taglist $latestStableUbuntu, $latestStableWsc1709, $latestStableWscLtsc, $latestStableWsc1803  -ManifestTag 'latest'
 &$createScriptPath -ContainerRegistry $Registry -taglist $latestStableNano1709, $latestStableNano1803  -ManifestTag 'nanoserver'
-&$createScriptPath -ContainerRegistry $Registry -taglist $latestStableNano1709, $latestStableNano1803  -ManifestTag "$stableVersion-nanoserver"
+&$createScriptPath -ContainerRegistry $Registry -taglist $latestStableNano1709, $latestStableNano1803  -ManifestTag "nanoserver"
 
 <# 
 NanoServer is broken in preview.3
 https://github.com/PowerShell/PowerShell/issues/6750
 
 &$createScriptPath -ContainerRegistry $Registry -taglist $latestPreviewNano1709, $latestPreviewNano1803  -ManifestTag 'nanoserver'
-&$createScriptPath -ContainerRegistry $Registry -taglist $latestPreviewNano1709, $latestPreviewNano1803  -ManifestTag "$stableVersion-nanoserver"
+&$createScriptPath -ContainerRegistry $Registry -taglist $latestPreviewNano1709, $latestPreviewNano1803  -ManifestTag "nanoserver"
 #> 
