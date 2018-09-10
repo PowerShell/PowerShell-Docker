5aa8fed8611d9fd339f91babb4135ca4e7263abf
Travis Plunk <travis.plunk@microsoft.com>
2018-09-10 16:22:43 -0700

    update stable files from preview folder

--- release/stable/ubuntu18.04/getLatestTag.ps1
+++ release/stable/ubuntu18.04/getLatestTag.ps1
@@ -1,14 +1,14 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-# return objects representing the tags we need to base the trusty image on
+# return objects representing the tags we need to base the beaver image on
 
-# The versions of trusty we care about
-$shortTags = @('trusty')
+# The versions of beaver we care about
+$shortTags = @('bionic')
 
 $parent = Join-Path -Path $PSScriptRoot -ChildPath '..'
 $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath '..'
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-Get-DockerTags -ShortTags $shortTags -Image "ubuntu" -FullTagFilter 'trusty-\d{8}[\.\d{1}]?' -AlternativeShortTag '14.04'
+Get-DockerTags -ShortTags $shortTags -Image "ubuntu" -FullTagFilter 'bionic-\d{8}[\.\d{1}]?' -AlternativeShortTag '18.04'
