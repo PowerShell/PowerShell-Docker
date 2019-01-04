28c6585d92f8152bb972c29a172bc0cecb42974d
Travis Plunk <travis.plunk@microsoft.com>
2019-01-03 16:31:53 -0800

    Update manifest for 1809 (#114)
    Update manifest for 1809
      - manifests are `preview`, `latest` and `nanoserver`
      - Also, add the ability to specify that some phases can just ignore all errors to temporarily deal with #112 and #113

--- createAllManifests.ps1
+++ createAllManifests.ps1
@@ -20,22 +20,25 @@ $latestStableUbuntu   = "ubuntu-bionic"
 $latestStableWsc1709  = "windowsservercore-1709"
 $latestStableWscLtsc  = "windowsservercore-latest"
 $latestStableWsc1803  = "windowsservercore-1803"
+$latestStableWsc1809  = "windowsservercore-1809"
 $latestStableNano1709 = "nanoserver-1709"
 $latestStableNano1803 = "nanoserver-1803"
+$latestStableNano1809 = "nanoserver-1809"
 
 $latestPreviewUbuntu   = "preview-ubuntu-bionic"
 $latestPreviewWsc1709  = "preview-windowsservercore-1709"
 $latestPreviewWscLtsc  = "preview-windowsservercore-latest"
 $latestPreviewWsc1803  = "preview-windowsservercore-1803"
+$latestPreviewWsc1809  = "preview-windowsservercore-1809"
 
 switch ($Channel)
 {
     'preview' {
-        &$createScriptPath -ContainerRegistry $Registry -taglist $latestPreviewUbuntu, $latestPreviewWsc1709, $latestPreviewWscLtsc, $latestPreviewWsc1803  -ManifestTag 'preview'
+        &$createScriptPath -ContainerRegistry $Registry -taglist $latestPreviewUbuntu, $latestPreviewWsc1709, $latestPreviewWscLtsc, $latestPreviewWsc1803, $latestPreviewWsc1809  -ManifestTag 'preview'
     }
 
     'stable' {
-        &$createScriptPath -ContainerRegistry $Registry -taglist $latestStableUbuntu, $latestStableWsc1709, $latestStableWscLtsc, $latestStableWsc1803  -ManifestTag 'latest'
-        &$createScriptPath -ContainerRegistry $Registry -taglist $latestStableNano1709, $latestStableNano1803  -ManifestTag 'nanoserver'
+        &$createScriptPath -ContainerRegistry $Registry -taglist $latestStableUbuntu, $latestStableWsc1709, $latestStableWscLtsc, $latestStableWsc1803, $latestStableWsc1809 -ManifestTag 'latest'
+        &$createScriptPath -ContainerRegistry $Registry -taglist $latestStableNano1709, $latestStableNano1803, $latestStableNano1809 -ManifestTag 'nanoserver'
     }
 }
