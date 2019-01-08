b2457dc1225035e246916245bbfda28ed876e92d
Travis Plunk <travis.plunk@microsoft.com>
2019-01-08 12:02:28 -0800

    Separate `nanoserver 1809` due to breaking change (#118)
    Separate `nanoserver 1809` due to breaking change
      - `SETX` no longer works to set environment variable so 1809 has to be separate at least from `1709` where `ENV` did not work for the `PATH` variable.
      - Also fix regex for `nanoserver` tag query
    
    Fixes #89

--- release/servicing/nanoserver/getLatestTag.ps1
+++ release/servicing/nanoserver/getLatestTag.ps1
@@ -6,7 +6,10 @@
 
 param(
     [Switch]
-    $CI
+    $CI,
+    # The versions of nanoserver we care about
+    [string[]]
+    $ShortTags
 )
 
 $parent = Join-Path -Path $PSScriptRoot -ChildPath '..'
@@ -14,17 +17,14 @@ $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-# The versions of nanoserver we care about
-$shortTags = @('1709','1803','1809')
-
 if(!$CI.IsPresent)
 {
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}_KB\d{7}'
+    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}_KB\d{7}(_amd64)?$' -Mcr
 }
 else {
     # This is not supported for nanoserver so don't build in production but try building it as a CI test for the Dockerfile
     $shortTags = @('latest')
 
     # The \d{4,} part of the regex is because the API is returning tags which are 3 digits and older than the 4 digit tags
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '10\.0\.14393\.\d{4,}$' -SkipShortTagFilter
+    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '10\.0\.14393\.\d{4,}$' -SkipShortTagFilter -Mcr
 }
