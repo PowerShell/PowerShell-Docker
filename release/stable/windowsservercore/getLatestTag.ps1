9eab61eb0959038703c67d3bf958da24533bec5b
Travis Plunk <github@ez13.net>
2019-02-20 14:20:41 -0800

    remove code to filter tags when using -CI

--- release/stable/windowsservercore/getLatestTag.ps1
+++ release/stable/windowsservercore/getLatestTag.ps1
@@ -13,15 +13,7 @@ $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath -Force
 
-if(!$CI.IsPresent)
-{
-    # The versions of nanoserver we care about
-    $shortTags = @('1709','1803','1809')
+# The versions of nanoserver we care about
+$shortTags = @('1709','1803','1809')
 
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}_KB\d{7}' -Mcr
-}
-
-$shortTags = @('latest')
-
-# The \d{4,} part of the regex is because the API is returning tags which have been deleted, which are 3 digits
-Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '10\.0\.14393\.\d{4,}$' -AlternativeShortTag 'ltsc2016' -SkipShortTagFilter -Mcr
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}_KB\d{7}' -Mcr
