PowerShell/PowerShell-Docker

commit be29925749ae7d01a7d25184a1dbf4f64a31cc1a

Refactor some of the build code into buildHelper (#157)
Refactor some of the build code into buildHelper to make the build more maintainable.

build.ps1
@@ -97,6 +97,8 @@ param(
     [Parameter(ParameterSetName="localBuildAll")]
     [Parameter(Mandatory, ParameterSetName="TestByName")]
     [Parameter(Mandatory, ParameterSetName="TestAll")]
+    [Parameter(Mandatory, ParameterSetName="GetTagsByName")]
+    [Parameter(Mandatory, ParameterSetName="GetTagsAll")]
     [ValidatePattern('(\d+\.){2}\d(-\w+(\.\d+)?)?')]
     [string]
     $Version,
@@ -144,21 +146,6 @@ DynamicParam {
     $dockerFileNames = @()
     foreach($imageChannel in $imageChannels){
         Get-ImageList -Channel $imageChannel | ForEach-Object { $dockerFileNames += $_ }
-    }
-
-    function Add-ParameterAttribute {
-        param(
-            [Parameter(Mandatory)]
-            [object]
-            $Attributes,
-            [Parameter(Mandatory)]
-            [string]
-            $ParameterSetName
-        )
-        $ParameterAttr = New-Object "System.Management.Automation.ParameterAttribute"
-        $ParameterAttr.ParameterSetName = $ParameterSetName
-        $ParameterAttr.Mandatory = $true
-        $Attributes.Add($ParameterAttr) > $null
     }
 
     # Create the parameter attributs
@@ -220,101 +207,33 @@ End {
             $Name = Get-ImageList -Channel $actualChannel
         }
 
-        $versionExtraParams = @{}
-        if($Version){
-            $versionExtraParams.Add('Version', $Version)
+        $versionExtraParams = @{
+            ServicingVersion = if($Version) {$Version} else {$ServicingVersion}
+            PreviewVersion = if($Version) {$Version} else {$PreviewVersion}
+            StableVersion = if($Version) {$Version} else {$StableVersion}
         }
 
-        switch -RegEx ($actualChannel)
-        {
-            'servicing$' {
-                if($ServicingVersion){
-                    $versionExtraParams['Version'] = $ServicingVersion
-                }
-
-                $windowsVersion = Get-PowerShellVersion -Servicing @versionExtraParams
-                $linuxVersion = Get-PowerShellVersion -Linux -Servicing @versionExtraParams
-            }
-            'preview$' {
-                if($PreviewVersion){
-                    $versionExtraParams['Version'] = $PreviewVersion
-                }
-
-                $windowsVersion = Get-PowerShellVersion -Preview @versionExtraParams
-                $linuxVersion = Get-PowerShellVersion -Linux -Preview @versionExtraParams
-            }
-            'stable$' {
-                if($StableVersion){
-                    $versionExtraParams['Version'] = $StableVersion
-                }
-
-                $windowsVersion = Get-PowerShellVersion @versionExtraParams
-                $linuxVersion = Get-PowerShellVersion -Linux @versionExtraParams
-            }
-            default {
-                throw "unknown channel: $Channel"
-            }
-        }
+        # Get Versions
+        $versions = Get-Versions -Channel $actualChannel @versionExtraParams
+        $windowsVersion = $versions.windowsVersion
+        $linuxVersion = $versions.linuxVersion
 
         # Calculate the paths
         $channelPath = Join-Path -Path $releasePath -ChildPath $actualChannel.ToLowerInvariant()
 
-
         foreach($dockerFileName in $Name)
         {
-            $imagePath = Join-Path -Path $channelPath -ChildPath $dockerFileName
-            $scriptPath = Join-Path -Path $imagePath -ChildPath 'getLatestTag.ps1'
-            $tagsJsonPath = Join-Path -Path $imagePath -ChildPath 'tags.json'
-            $metaJsonPath = Join-Path -Path $imagePath -ChildPath 'meta.json'
-
-            # skip an image if it doesn't exist
-            if(!(Test-Path $scriptPath))
-            {
-                $message = "Channel: $actualChannel, Name: $dockerFileName does not existing.  Not every image exists in every channel.  Skipping."
-                if($CI.IsPresent)
-                {
-                    throw $message
-                }
-
-                Write-Warning $message
-                continue
-            }
-
-            $meta = Get-DockerImageMetaData -Path $metaJsonPath
-            if($meta.tagTemplates.count -gt 0)
-            {
-                $tagsTemplates = $meta.tagTemplates
-            }
-            else
-            {
-                $tagsTemplates = Get-Content -Path $tagsJsonPath | ConvertFrom-Json
-            }
+            # Get all image meta data
+            $allMeta = Get-DockerImageMetaDataWrapper -DockerFileName $dockerFileName -CI:$CI.IsPresent -IncludeKnownIssues:$IncludeKnownIssues.IsPresent -ChannelPath $channelPath
+            $meta = $allMeta.meta
+            $tagsTemplates = $allMeta.tagsTemplates
+            $imagePath = $allMeta.imagePath
+            $tagData = $allMeta.tagData
 
             $psversion = $windowsVersion
             if($meta.ShouldUseLinuxVersion())
             {
                 $psversion = $linuxVersion
-            }
-
-            $getTagsExtraParams = @{}
-
-            if($meta.ShortTags.count -gt 0)
-            {
-                $shortTags = @()
-                foreach ($shortTag in $meta.ShortTags) {
-                    if(!$shortTag.KnownIssue -or $IncludeKnownIssues.IsPresent)
-                    {
-                        $shortTags += $shortTag.Tag
-                    }
-                }
-
-                $getTagsExtraParams.Add('ShortTags',$shortTags)
-            }
-            # Get the tag data for the image
-            $tagData = @(& $scriptPath -CI:$CI.IsPresent @getTagsExtraParams | Where-Object {$_.FromTag})
-            if($TagFilter)
-            {
-                $tagData = $tagData | Where-Object { $_.FromTag -match $TagFilter }
             }
 
             foreach ($tagGroup in ($tagData | Group-Object -Property 'FromTag')) {
@@ -568,10 +487,13 @@ End {
 
     if($CheckForDuplicateTags.IsPresent)
     {
-        Write-Verbose "checking dup issues" -Verbose
         if($dupeTagIssues.count -gt 0)
         {
             throw ($dupeTagIssues -join [System.Environment]::NewLine)
+        }
+        else
+        {
+            Write-Verbose "No duplicates found." -Verbose
         }
     }
 }

tools/buildHelper/buildHelper.psm1
@@ -224,3 +224,154 @@ function script:Start-NativeExecution
         }
     }
 }
+
+# Add a parameter attribute to an Attribute Collection
+function Add-ParameterAttribute {
+    param(
+        [Parameter(Mandatory)]
+        [object]
+        $Attributes,
+        [Parameter(Mandatory)]
+        [string]
+        $ParameterSetName
+    )
+    $ParameterAttr = New-Object "System.Management.Automation.ParameterAttribute"
+    $ParameterAttr.ParameterSetName = $ParameterSetName
+    $ParameterAttr.Mandatory = $true
+    $Attributes.Add($ParameterAttr) > $null
+}
+
+function Get-Versions
+{
+    param(
+        [Parameter(Mandatory)]
+        [String]
+        $Channel,
+
+        [string]
+        $ServicingVersion,
+
+        [string]
+        $PreviewVersion,
+
+        [string]
+        $StableVersion
+    )
+
+    Write-Verbose "Getting Version for $Channel - servicing: $ServicingVersion; Preview: $PreviewVersion; stable: $stableVersion"
+
+    $versionExtraParams = @{}
+
+    switch -RegEx ($Channel)
+    {
+        'servicing$' {
+            if($ServicingVersion){
+                $versionExtraParams['Version'] = $ServicingVersion
+            }
+
+            $windowsVersion = Get-PowerShellVersion -Servicing @versionExtraParams
+            $linuxVersion = Get-PowerShellVersion -Linux -Servicing @versionExtraParams
+        }
+        'preview$' {
+            if($PreviewVersion){
+                $versionExtraParams['Version'] = $PreviewVersion
+            }
+
+            $windowsVersion = Get-PowerShellVersion -Preview @versionExtraParams
+            $linuxVersion = Get-PowerShellVersion -Linux -Preview @versionExtraParams
+        }
+        'stable$' {
+            if($StableVersion){
+                $versionExtraParams['Version'] = $StableVersion
+            }
+
+            $windowsVersion = Get-PowerShellVersion @versionExtraParams
+            $linuxVersion = Get-PowerShellVersion -Linux @versionExtraParams
+        }
+        default {
+            throw "unknown channel: $Channel"
+        }
+    }
+
+    return [PSCustomObject] @{
+        WindowsVersion = $windowsVersion
+        LinuxVersion = $linuxVersion
+    }
+}
+
+# Get the meta data and the tag data for an image
+function Get-DockerImageMetaDataWrapper
+{
+    param(
+        [parameter(Mandatory)]
+        [string]
+        $DockerFileName,
+
+        [switch]
+        $CI,
+
+        [switch]
+        $IncludeKnownIssues,
+
+        [parameter(Mandatory)]
+        [string]
+        $ChannelPath
+    )
+
+    $imagePath = Join-Path -Path $ChannelPath -ChildPath $dockerFileName
+    $scriptPath = Join-Path -Path $imagePath -ChildPath 'getLatestTag.ps1'
+    $tagsJsonPath = Join-Path -Path $imagePath -ChildPath 'tags.json'
+    $metaJsonPath = Join-Path -Path $imagePath -ChildPath 'meta.json'
+
+    # skip an image if it doesn't exist
+    if(!(Test-Path $scriptPath))
+    {
+        $message = "Channel: $actualChannel, Name: $dockerFileName does not existing.  Not every image exists in every channel.  Skipping."
+        if($CI.IsPresent)
+        {
+            throw $message
+        }
+
+        Write-Warning $message
+        continue
+    }
+
+    $meta = Get-DockerImageMetaData -Path $metaJsonPath
+    if($meta.tagTemplates.count -gt 0)
+    {
+        $tagsTemplates = $meta.tagTemplates
+    }
+    else
+    {
+        $tagsTemplates = Get-Content -Path $tagsJsonPath | ConvertFrom-Json
+    }
+
+    $getTagsExtraParams = @{}
+
+    if($meta.ShortTags.count -gt 0)
+    {
+        $shortTags = @()
+        foreach ($shortTag in $meta.ShortTags) {
+            if(!$shortTag.KnownIssue -or $IncludeKnownIssues.IsPresent)
+            {
+                $shortTags += $shortTag.Tag
+            }
+        }
+
+        $getTagsExtraParams.Add('ShortTags',$shortTags)
+    }
+
+    # Get the tag data for the image
+    $tagData = @(& $scriptPath -CI:$CI.IsPresent @getTagsExtraParams | Where-Object {$_.FromTag})
+    if($TagFilter)
+    {
+        $tagData = $tagData | Where-Object { $_.FromTag -match $TagFilter }
+    }
+
+    return [PSCustomObject]@{
+        meta = $meta
+        tagsTemplates = $tagsTemplates
+        imagePath = $imagePath
+        tagData = $tagData
+    }
+}

commit f25b5d568257aaad222a13f5c4b70c401ebd28ac

Fix the `-TagFilter` to `build.ps1` which regressed in #157 (#158)
Fix the `-TagFilter` to `build.ps1` which regressed in #157
  - Also, use TagFilter in CI to make sure that we don't regress the feature again and use a more relevant version of windows

.vsts-ci/manifestSteps.yml
@@ -4,20 +4,20 @@ steps:
     Write-Host "##vso[task.setvariable variable=dockerNamespace;]$namespace"
   displayName: 'Set dockerNamespace'
 
-- powershell: 'Get-ChildItem env:' 
+- powershell: 'Get-ChildItem env:'
   displayName: 'Capture Environment'
 
-- powershell: |  
+- powershell: |
       $dockerConfigFolder = "$env:userprofile/.docker"
       if(!(Test-Path $dockerConfigFolder)){ $null = new-item -Type Directory -Path $dockerConfigFolder}
       $dockerCliConfig = "$env:userprofile/.docker/config.json"
       $dockerCliBackup = "$env:userprofile/.docker/config-backup.json"
       if(Test-Path $dockerCliConfig) { copy-item $dockerCliConfig $dockerCliBackup -force}
       @{experimental='enabled'}|ConvertTo-Json | Out-File -Encoding ascii -FilePath $dockerCliConfig
-      
+
   displayName: 'enable docker cli experimental features'
 
-- powershell: 'docker login $(dockerHost) -u $(dockerUserName) -p $(dockerKey)' 
+- powershell: 'docker login $(dockerHost) -u $(dockerUserName) -p $(dockerKey)'
   displayName: 'docker login'
 
 - powershell: |
@@ -31,6 +31,12 @@ steps:
       }
   displayName: 'Create $(Channel) Manifest Lists'
 
-- powershell: 'docker logout $(dockerHost)' 
+- powershell: 'docker logout $(dockerHost)'
   displayName: 'docker logout'
   condition: always()
+
+- task: ms.vss-governance-buildtask.governance-build-task-component-detection.ComponentGovernanceComponentDetection@0
+  displayName: 'Component Detection'
+  inputs:
+    sourceScanPath: '$(Build.SourcesDirectory)'
+    snapshotForceEnabled: true

.vsts-ci/phase.yml
@@ -9,6 +9,7 @@ parameters:
   communityPreview: 'false'
   communityServicing: 'false'
   continueonerror: false
+  ciParameter: '-CI'
 
 phases:
 - phase: ${{ parameters.name }}
@@ -23,37 +24,37 @@ phases:
     condition: succeededOrFailed()
 
   - ${{ if eq(parameters.stable, 'true') }}:
-    - powershell: ./build.ps1 -build -name '$(ImageName)' -CI -IncludeKnownIssues -Channel 'stable' -TestLogPostfix '$(ImageName)-stable'
+    - powershell: ./build.ps1 -build -name '$(ImageName)' -IncludeKnownIssues -Channel 'stable' -TestLogPostfix '$(ImageName)-stable' ${{ parameters.ciParameter }}
       displayName: $(ImageName) Stable
       condition: succeededOrFailed()
       continueOnError: ${{ parameters.continueonerror }}
 
   - ${{ if eq(parameters.preview, 'true') }}:
-    - powershell: ./build.ps1 -build -name '$(ImageName)' -CI -IncludeKnownIssues -Channel 'preview' -TestLogPostfix '$(ImageName)-preview'
+    - powershell: ./build.ps1 -build -name '$(ImageName)' -IncludeKnownIssues -Channel 'preview' -TestLogPostfix '$(ImageName)-preview' ${{ parameters.ciParameter }}
       displayName: $(ImageName) Preview
       condition: succeededOrFailed()
       continueOnError: ${{ parameters.continueonerror }}
 
   - ${{ if eq(parameters.servicing, 'true') }}:
-    - powershell: ./build.ps1 -build -name '$(ImageName)' -CI -IncludeKnownIssues -Channel 'servicing' -TestLogPostfix '$(ImageName)-servicing'
+    - powershell: ./build.ps1 -build -name '$(ImageName)' -IncludeKnownIssues -Channel 'servicing' -TestLogPostfix '$(ImageName)-servicing' ${{ parameters.ciParameter }}
       displayName: $(ImageName) Servicing
       condition: succeededOrFailed()
       continueOnError: ${{ parameters.continueonerror }}
 
   - ${{ if eq(parameters.communityStable, 'true') }}:
-    - powershell: ./build.ps1 -build -name '$(ImageName)' -CI -IncludeKnownIssues -Channel 'community-stable' -TestLogPostfix '$(ImageName)-stable'
+    - powershell: ./build.ps1 -build -name '$(ImageName)' -IncludeKnownIssues -Channel 'community-stable' -TestLogPostfix '$(ImageName)-stable' ${{ parameters.ciParameter }}
       displayName: $(ImageName) Stable
       condition: succeededOrFailed()
       continueOnError: ${{ parameters.continueonerror }}
 
   - ${{ if eq(parameters.communityPreview, 'true') }}:
-    - powershell: ./build.ps1 -build -name '$(ImageName)' -CI -IncludeKnownIssues -Channel 'community-preview' -TestLogPostfix '$(ImageName)-preview'
+    - powershell: ./build.ps1 -build -name '$(ImageName)' -IncludeKnownIssues -Channel 'community-preview' -TestLogPostfix '$(ImageName)-preview' ${{ parameters.ciParameter }}
       displayName: $(ImageName) Preview
       condition: succeededOrFailed()
       continueOnError: ${{ parameters.continueonerror }}
 
   - ${{ if eq(parameters.communityServicing, 'true') }}:
-    - powershell: ./build.ps1 -build -name '$(ImageName)' -CI -IncludeKnownIssues -Channel 'community-servicing' -TestLogPostfix '$(ImageName)-servicing'
+    - powershell: ./build.ps1 -build -name '$(ImageName)' -IncludeKnownIssues -Channel 'community-servicing' -TestLogPostfix '$(ImageName)-servicing' ${{ parameters.ciParameter }}
       displayName: $(ImageName) Servicing
       condition: succeededOrFailed()
       continueOnError: ${{ parameters.continueonerror }}

.vsts-ci/releasePhase.yml
@@ -87,3 +87,9 @@ jobs:
 
   - powershell: 'az logout'
     displayName: 'az logout'
+
+  - task: ms.vss-governance-buildtask.governance-build-task-component-detection.ComponentGovernanceComponentDetection@0
+    displayName: 'Component Detection'
+    inputs:
+      sourceScanPath: '$(Build.SourcesDirectory)'
+      snapshotForceEnabled: true

.vsts-ci/releasebuild.yml
@@ -112,3 +112,9 @@ jobs:
       }|convertto-json | out-file -FilePath $path -Encoding ascii
       Write-Host "##vso[artifact.upload containerfolder=releaseTags;artifactname=releaseTags]$path"
     displayName: Save release Tags
+
+  - task: ms.vss-governance-buildtask.governance-build-task-component-detection.ComponentGovernanceComponentDetection@0
+    displayName: 'Component Detection'
+    inputs:
+      sourceScanPath: '$(Build.SourcesDirectory)'
+      snapshotForceEnabled: true

build.ps1
@@ -224,7 +224,13 @@ End {
         foreach($dockerFileName in $Name)
         {
             # Get all image meta data
-            $allMeta = Get-DockerImageMetaDataWrapper -DockerFileName $dockerFileName -CI:$CI.IsPresent -IncludeKnownIssues:$IncludeKnownIssues.IsPresent -ChannelPath $channelPath
+            $allMeta = Get-DockerImageMetaDataWrapper `
+                -DockerFileName $dockerFileName `
+                -CI:$CI.IsPresent `
+                -IncludeKnownIssues:$IncludeKnownIssues.IsPresent `
+                -ChannelPath $channelPath `
+                -TagFilter $TagFilter
+
             $meta = $allMeta.meta
             $tagsTemplates = $allMeta.tagsTemplates
             $imagePath = $allMeta.imagePath

release/preview/nanoserver1809/getLatestTag.ps1
@@ -17,14 +17,4 @@ $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-if(!$CI.IsPresent)
-{
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}_KB\d{7}(_amd64)?$' -Mcr
-}
-else {
-    # This is not supported for nanoserver so don't build in production but try building it as a CI test for the Dockerfile
-    $shortTags = @('latest')
-
-    # The \d{4,} part of the regex is because the API is returning tags which are 3 digits and older than the 4 digit tags
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '10\.0\.14393\.\d{4,}$' -SkipShortTagFilter -Mcr
-}
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}_KB\d{7}(_amd64)?$' -Mcr

release/preview/windowsservercore/getLatestTag.ps1
@@ -13,15 +13,7 @@ $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath -Force
 
-if(!$CI.IsPresent)
-{
-    # The versions of nanoserver we care about
-    $shortTags = @('1709','1803','1809')
+# The versions of nanoserver we care about
+$shortTags = @('1709','1803','1809')
 
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}_KB\d{7}' -Mcr
-}
-
-$shortTags = @('latest')
-
-# The \d{4,} part of the regex is because the API is returning tags which have been deleted, which are 3 digits
-Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '10\.0\.14393\.\d{4,}$' -AlternativeShortTag 'ltsc2016' -SkipShortTagFilter -Mcr
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}_KB\d{7}' -Mcr

release/servicing/nanoserver1809/getLatestTag.ps1
@@ -17,14 +17,4 @@ $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-if(!$CI.IsPresent)
-{
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}_KB\d{7}(_amd64)?$' -Mcr
-}
-else {
-    # This is not supported for nanoserver so don't build in production but try building it as a CI test for the Dockerfile
-    $shortTags = @('latest')
-
-    # The \d{4,} part of the regex is because the API is returning tags which are 3 digits and older than the 4 digit tags
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '10\.0\.14393\.\d{4,}$' -SkipShortTagFilter -Mcr
-}
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}_KB\d{7}(_amd64)?$' -Mcr

release/servicing/windowsservercore/getLatestTag.ps1
@@ -13,15 +13,7 @@ $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath -Force
 
-if(!$CI.IsPresent)
-{
-    # The versions of nanoserver we care about
-    $shortTags = @('1709','1803','1809')
+# The versions of nanoserver we care about
+$shortTags = @('1709','1803','1809')
 
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}_KB\d{7}' -Mcr
-}
-
-$shortTags = @('latest')
-
-# The \d{4,} part of the regex is because the API is returning tags which have been deleted, which are 3 digits
-Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '10\.0\.14393\.\d{4,}$' -AlternativeShortTag 'ltsc2016' -SkipShortTagFilter -Mcr
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}_KB\d{7}' -Mcr

release/stable/nanoserver1809/getLatestTag.ps1
@@ -17,14 +17,4 @@ $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-if(!$CI.IsPresent)
-{
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}_KB\d{7}(_amd64)?$' -Mcr
-}
-else {
-    # This is not supported for nanoserver so don't build in production but try building it as a CI test for the Dockerfile
-    $shortTags = @('latest')
-
-    # The \d{4,} part of the regex is because the API is returning tags which are 3 digits and older than the 4 digit tags
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '10\.0\.14393\.\d{4,}$' -SkipShortTagFilter -Mcr
-}
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}_KB\d{7}(_amd64)?$' -Mcr

release/stable/windowsservercore/getLatestTag.ps1
@@ -13,15 +13,7 @@ $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath -Force
 
-if(!$CI.IsPresent)
-{
-    # The versions of nanoserver we care about
-    $shortTags = @('1709','1803','1809')
+# The versions of nanoserver we care about
+$shortTags = @('1709','1803','1809')
 
-    Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}_KB\d{7}' -Mcr
-}
-
-$shortTags = @('latest')
-
-# The \d{4,} part of the regex is because the API is returning tags which have been deleted, which are 3 digits
-Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '10\.0\.14393\.\d{4,}$' -AlternativeShortTag 'ltsc2016' -SkipShortTagFilter -Mcr
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}_KB\d{7}' -Mcr

tools/buildHelper/buildHelper.psm1
@@ -313,6 +313,9 @@ function Get-DockerImageMetaDataWrapper
         [switch]
         $IncludeKnownIssues,
 
+        [string]
+        $TagFilter,
+
         [parameter(Mandatory)]
         [string]
         $ChannelPath

vsts-ci.yml
@@ -148,20 +148,23 @@ phases:
     preview: false
     communityStable: true
 
-# Use CI switch to disable 1709 and 1803 builds because the VSTS hosted instances don't yet support 1709 or 1803
+# The -CI filters to LTSC-2016 for nanoserver by default, which is the only thing that works on Hosted VS2017
 # Skipping previews on windows due to how long it takes to build on windows
 - template: .vsts-ci/phase.yml
   parameters:
-    name: nanoserver
+    name: nanoserver_ltsc_2016
     imagename: nanoserver
     queue: Hosted VS2017
     servicing: false
     preview: false
+    ciParameter: '-CI'
 
+# Use the TagFilter to filter to 1803 because the docker instance inside the container only supports 1803
 - template: .vsts-ci/phase.yml
   parameters:
-    name: windowsservercore
-    queue: Hosted VS2017
+    name: windowsservercore_1803
+    queue: Hosted Windows Container
     imagename: windowsservercore
     servicing: false
     preview: false
+    ciParameter: '-CI -TagFilter 1803'