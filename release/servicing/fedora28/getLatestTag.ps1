fcdf2b9604252542db1f8b0d53ce80a495613af1
Travis Plunk <github@ez13.net>
2019-03-25 13:58:51 -0700

    move servicing to stable

--- release/servicing/fedora28/getLatestTag.ps1
+++ release/servicing/fedora28/getLatestTag.ps1
@@ -1,14 +1,14 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-# return objects representing the tags we need to base the CentOS image on Docker
+# return objects representing the tags we need to base the fedora image on
 
-# The versions of CentOS we care about
-$shortTags = @('7')
+# The versions of fedora we care about
+$shortTags = @('28')
 
 $parent = Join-Path -Path $PSScriptRoot -ChildPath '..'
 $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath '..'
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-Get-DockerTags -ShortTags $shortTags -Image "centos" -FullTagFilter '^7$' -OnlyShortTags
+Get-DockerTags -ShortTags $shortTags -Image "fedora" -FullTagFilter '^28$' -OnlyShortTags
--- release/servicing/ubuntu18.04/getLatestTag.ps1
+++ release/servicing/ubuntu18.04/getLatestTag.ps1
@@ -1,14 +1,14 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-# return objects representing the tags we need to base the CentOS image on Docker
+# return objects representing the tags we need to base the beaver image on
 
-# The versions of CentOS we care about
-$shortTags = @('7')
+# The versions of beaver we care about
+$shortTags = @('bionic')
 
 $parent = Join-Path -Path $PSScriptRoot -ChildPath '..'
 $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath '..'
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-Get-DockerTags -ShortTags $shortTags -Image "centos" -FullTagFilter '^7$' -OnlyShortTags
+Get-DockerTags -ShortTags $shortTags -Image "ubuntu" -FullTagFilter 'bionic-\d{8}[\.\d{1}]?' -AlternativeShortTag '18.04'
