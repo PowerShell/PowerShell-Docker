09bb842990ff44658e92477f9cc12b649e334605
Travis Plunk <github@ez13.net>
2019-03-25 14:10:25 -0700

    move stable to servicing

--- release/servicing/nanoserver1809/getLatestTag.ps1
+++ release/servicing/nanoserver1809/getLatestTag.ps1
@@ -1,14 +1,20 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-# return objects representing the tags we need to base the CentOS image on Docker
+# return objects representing the tags we need to base the nanoserver image on
 
-# The versions of CentOS we care about
-$shortTags = @('7')
+
+param(
+    [Switch]
+    $CI,
+    # The versions of nanoserver we care about
+    [string[]]
+    $ShortTags
+)
 
 $parent = Join-Path -Path $PSScriptRoot -ChildPath '..'
 $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath '..'
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-Get-DockerTags -ShortTags $shortTags -Image "centos" -FullTagFilter '^7$' -OnlyShortTags
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}_KB\d{7}(_amd64)?$' -Mcr
