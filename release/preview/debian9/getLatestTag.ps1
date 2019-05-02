4c7ded15d8bd04fc069eddb5d77e3bae6a0a4d7b
Reece Dunham <me@rdil.rocks>
2019-05-02 14:48:13 -0400

    Update getLatestTag.ps1

--- release/preview/debian9/getLatestTag.ps1
+++ release/preview/debian9/getLatestTag.ps1
@@ -4,11 +4,11 @@
 # return objects representing the tags we need to base the debian image on Docker
 
 # The versions of debian we care about
-$shortTags = @('stretch')
+$shortTags = @('stretch-slim')
 
 $parent = Join-Path -Path $PSScriptRoot -ChildPath '..'
 $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath '..'
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-Get-DockerTags -ShortTags $shortTags -Image "debian" -FullTagFilter 'stretch-\d{8}[\.\d{1}]?' -AlternativeShortTag '9'
+Get-DockerTags -ShortTags $shortTags -Image "debian" -FullTagFilter 'stretch-\d{8}[\.\d{1}]?-slim' -AlternativeShortTag '9'
