6516891b5b715b70b149e10a174767312b0d4d86
Travis Plunk <github@ez13.net>
2019-06-30 12:57:49 -0700

    fix breaking changes to tag format

--- release/preview/windowsservercore/getLatestTag.ps1
+++ release/preview/windowsservercore/getLatestTag.ps1
@@ -16,4 +16,4 @@ Import-Module $modulePath -Force
 # The versions of nanoserver we care about
 $shortTags = @('1803','1809','1903')
 
-Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}_KB\d{7}' -Mcr
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}[-_]KB\d{7}' -Mcr
