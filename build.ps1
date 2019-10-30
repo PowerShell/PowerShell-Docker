4c04140ca911fd3c4e925f75e5219158da6b9052
Reece Dunham <me@rdil.rocks>
2019-10-30 15:33:01 -0400

    Update build.ps1
    Co-Authored-By: Travis Plunk <travis.plunk@microsoft.com>

--- build.ps1
+++ build.ps1
@@ -412,7 +412,7 @@ End {
             $extraParams.Add('Tags', $tags)
         }
 
-        if((Get-Module -ListAvailable pester -ErrorAction Ignore -Force) -or !$ForcePesterInstall.IsPresent)
+        if((Get-Module -ListAvailable pester -ErrorAction Ignore -Force) -or $ForcePesterInstall.IsPresent)
         {
             Install-Module -Name pester -Scope CurrentUser -Force
         }
