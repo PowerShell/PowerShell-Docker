c17d7bf61386c97f6b4c0d5fc3b72e46fd29e502
Reece Dunham <me@rdil.rocks>
2019-11-04 14:28:34 -0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/pwsh
-# Copyright (c) Microsoft Corporation.
+# Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
 # Queue Docker image build for a particular image
