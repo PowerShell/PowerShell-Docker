a73686db3f68f89ecbf2c2fbf8311c5fc93e6803
Travis Plunk <travis@ez13.net>
2020-04-15 13:42:36 -0700

    fix entrypoint

--- release/preview/ubuntu18.04/docker/pwsh-entrypoint.sh
+++ release/preview/ubuntu18.04/docker/pwsh-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # first arg is `pwsh` or similar
-if ! { [ $1 = 'pwsh' ] || [ $1 = 'pwsh-preview' ]; }; then
+if ! { [ "$1" = 'pwsh' ] || [ "$1" = 'pwsh-preview' ] || [ "$1" = '/bin/bash' ] || [ "$1" = '/bin/sh' ] || [ "$1" = 'bash' ] || [ "$1" = 'sh' ]; }; then
  set -- pwsh -nologo -l -c "$@"
 fi
 
