96ccb39e278bc78b18ca2ad39319215c2dba0850
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2020-07-23 04:20:18 +1000

    Move shortTags from getLatestTag.ps1 to meta.json
    Signed-off-by: Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>

--- release/preview/windowsservercore/getLatestTag.ps1
+++ release/preview/windowsservercore/getLatestTag.ps1
@@ -3,17 +3,18 @@
 
 # return objects representing the tags we need to base the nanoserver image on
 
+
 param(
     [Switch]
-    $CI
+    $CI,
+    # The versions of nanoserver we care about
+    [string[]]
+    $ShortTags
 )
 
 $parent = Join-Path -Path $PSScriptRoot -ChildPath '..'
 $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath '..'
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
-Import-Module $modulePath -Force
-
-# The versions of nanoserver we care about
-$shortTags = @('1809', '1903', '1909', '2004')
+Import-Module $modulePath
 
 Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/servercore" -FullTagFilter '\d{4}[-_]KB\d{7}' -Mcr
