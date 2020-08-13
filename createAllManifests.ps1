5154d4cd9cc4fac2276216316353f3d1be648b29
xtqqczze <45661989+xtqqczze@users.noreply.github.com>
2020-08-13 19:03:24 +0100

    Remove All rights reserved term in copyright header (#480)

--- createAllManifests.ps1
+++ createAllManifests.ps1
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation. 
 # Licensed under the MIT License.
 
 # script to create the Docker manifest lists
--- release/stable/alpine38/getLatestTag.ps1
+++ release/stable/alpine38/getLatestTag.ps1
@@ -1,4 +1,4 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
+# Copyright (c) Microsoft Corporation. 
 # Licensed under the MIT License.
 
 # return objects representing the tags we need to base the Alpine image on
