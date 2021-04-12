890b315813c83ca55dfe9bab209abfed552a9c04
Vincent Boulineau <58430298+vboulineau@users.noreply.github.com>
2021-04-12 21:42:29 +0200

    Add Windows 20H2 images (#523)
    Signed-off-by: Vincent Boulineau <vincent.boulineau@datadoghq.com>

--- release/servicing/nanoserver1809/getLatestTag.ps1
+++ release/servicing/nanoserver1809/getLatestTag.ps1
@@ -17,5 +17,5 @@ $repoRoot = Join-Path -path (Join-Path -Path $parent -ChildPath '..') -ChildPath
 $modulePath = Join-Path -Path $repoRoot -ChildPath 'tools\getDockerTags'
 Import-Module $modulePath
 
-Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '\d{4}[-_]KB\d{7}([\-_]amd64)?$' -Mcr
+Get-DockerTags -ShortTags $shortTags -Image "mcr.microsoft.com/windows/nanoserver" -FullTagFilter '(\d|H){4}[-_]KB\d{7}([\-_]amd64)?$' -Mcr
 
