dc2ee265ff403301490addb86670570673f653c4
Travis Plunk <github@ez13.net>
2022-05-31 17:27:10 -0700

    Make matrix JSON consistent

---
+++ tools/updateJson.ps1
@@ -0,0 +1,58 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+$diffFound = $false
+$jsonPath = "$PSScriptRoot\..\assets\matrix.json"
+$json = &"$PSScriptRoot\..\build.ps1" -GenerateMatrixJson -FullJson
+$newMatrix = $json | ConvertFrom-Json
+$oldMatrix = Get-Content -Raw $jsonPath | ConvertFrom-Json
+$newChannels = $newMatrix | Get-Member -Type NoteProperty  | Select-Object -ExpandProperty Name | Sort-Object
+$oldChannels = $oldMatrix | Get-Member -Type NoteProperty  | Select-Object -ExpandProperty Name | Sort-Object
+if (Compare-Object $newChannels -DifferenceObject $oldChannels) {
+    $diffFound = $true
+}
+
+if (!$diffFound) {
+    foreach ($channel in $newChannels) {
+        $newImages = $newMatrix.$channel
+        $oldImages = $oldMatrix.$channel
+        if ($newImages.Count -ne $oldImages.Count) {
+            Write-Verbose -message "diff found in ${channel} image count. $($newImages.Count) : $($oldImages.Count)" -Verbose
+            $diffFound = $true
+            break
+        }
+        $newImageNames = $newImages | Select-Object -ExpandProperty JobName | Sort-Object
+        foreach ($imageName in $newImageNames) {
+            Write-Verbose -Message "Checking ${channel}/$($imageName)" -Verbose
+            $newImage = $newImages | Where-Object { $_.JobName -eq $imageName }
+            $oldImage = $oldImages | Where-Object { $_.JobName -eq $imageName }
+            if (!$newImage) {
+                throw "new image not found: $imageName"
+            }
+            $properties = $newImage | get-member -Type NoteProperty | Select-Object -ExpandProperty Name
+            foreach($property in $properties) {
+                if($newImage.$property -ne $oldImage.$property) {
+                    Write-Verbose -Message "diff found in ${channel}/$($imageName)/$($property)" -Verbose
+                    $diffFound = $true
+                    break
+                }
+            }
+            if ($diffFound) {
+                Write-Verbose -Message "breaking in ${channel}/$($imageName)" -Verbose
+                break
+            }
+        }
+        if ($diffFound) {
+            Write-Verbose -Message "breaking in ${channel}" -Verbose
+            break
+        }
+    }
+}
+
+if ($diffFound) {
+    Write-Verbose -message "diff found" -Verbose
+    $json > $jsonPath
+}
+else {
+    Write-Verbose -Message "no diff found" -Verbose
+}
