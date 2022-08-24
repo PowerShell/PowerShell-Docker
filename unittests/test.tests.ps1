999fd62929bd3bcc3d317e3cb84559d1de71ee3c
Anam Navied <anam.naviyou@gmail.com>
2022-08-24 13:08:11 -0400

    change unittests to use current preview and stable versions instead of hardcoded 7.2.2

--- unittests/test.tests.ps1
+++ unittests/test.tests.ps1
@@ -6,24 +6,29 @@ Describe "build.ps1 -Test" {
         $buildScript = Join-Path $repoRoot "build.ps1"
         Write-Verbose "buildScript: $buildScript"
         Import-Module -Name "$repoRoot/tools/buildHelper/buildHelper.psm1" -Force
+
+        # get versions for current preview and stable channel
+        $previewVersion = Get-PowerShellVersion -Preview
+        $stableVersion = Get-PowerShellVersion
+
         Mock -CommandName "Invoke-PesterWrapper" -MockWith {
             Write-Verbose "Mocking Invoke-PesterWrapper" -Verbose
         } -ParameterFilter { $ExtraParams -ne $null}
     }
     Context "-Channel -Name" {
         It "Should invoke pester" {
-            & $buildScript -Test -Pull -ImageName "unittest" -Name 'ubuntu22.04' -Channel preview -Version v7.2.2 4>&1 | out-null
+            & $buildScript -Test -Pull -ImageName "unittest" -Name 'ubuntu22.04' -Channel preview -Version $previewVersion 4>&1 | out-null
             Assert-VerifiableMock
         }
         It "throw for non existant image" {
             [Type] $exceptionType = [System.Management.Automation.ParameterBindingException]
-            {& $buildScript -Test -Pull -ImageName "unittest" -Name 'doesnt exist' -Channel preview -Version v7.2.2 4>&1} | Should -Throw -ExceptionType $exceptionType
+            {& $buildScript -Test -Pull -ImageName "unittest" -Name 'doesnt exist' -Channel preview -Version $previewVersion 4>&1} | Should -Throw -ExceptionType $exceptionType
             Assert-VerifiableMock
         }
     }
     Context "-channel -all" {
         It "Should invoke pester" {
-            & $buildScript -Test -channel stable -all -Version v7.2.2 4>&1 | out-null
+            & $buildScript -Test -channel stable -all -Version $stableVersion 4>&1 | out-null
             Assert-VerifiableMock
         }
     }
