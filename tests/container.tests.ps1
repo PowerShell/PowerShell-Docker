0a68c2690aa5f24f22e304c47758d0de7b6b5de2
travis plunk <github@ez13.net>
2022-10-06 10:06:01 -0700

    Mark size test as pending

--- tests/container.tests.ps1
+++ tests/container.tests.ps1
@@ -580,7 +580,8 @@ Describe "Linux Containers" -Tags 'Behavior', 'Linux' {
             })
         }
 
-        it "Verify size of <name>" -TestCases $sizeTestCases  -Skip:$script:skipLinuxRun {
+        # -Skip:$script:skipLinuxRun
+        it "Verify size of <name>" -TestCases $sizeTestCases -Pending {
             param(
                 [Parameter(Mandatory=$true)]
                 [string]
