c3902f4efc9284edb959eedc557353569f4c913b
travis plunk <github@ez13.net>
2022-10-06 09:57:55 -0700

    Fix tests

--- tests/container.tests.ps1
+++ tests/container.tests.ps1
@@ -505,7 +505,6 @@ Describe "Linux Containers" -Tags 'Behavior', 'Linux' {
             $muslCommands = @(
                 @{
                     command = 'node'
-                    path = '/usr/local/bin/node'
                 }
             )
 
